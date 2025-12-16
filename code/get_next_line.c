9267a9816c66e55e31941000f7f539ae6897467b
Suhail <suhail542@gmail.com>
2025-12-16 22:05:57 +0400

    tracing eof identification

--- code/get_next_line.c
+++ code/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ssujaude <ssujaude@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 15:17:45 by ssujaude          #+#    #+#             */
-/*   Updated: 2025/12/16 21:22:24 by ssujaude         ###   ########.fr       */
+/*   Updated: 2025/12/16 22:05:30 by ssujaude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,23 @@
 
 
 
-char *refine_line(char *old_stashed_data, char **full_line)
+char *refine_line(char *old_stashed_data, char **full_line, int old_stash_len)
 {
 	char *new_stashed_data;
 	int i;
 	int line_length;
+	char *terminator_point;
 
-	line_length = ft_strchr(old_stashed_data, '\n') - old_stashed_data;
-	if(line_length > 0)
+	if(old_stashed_data)
 	{
+		terminator_point = ft_strchr(old_stashed_data, '\n');
+		// if no \n... indicating \0 EOF existance
+		if(!terminator_point)
+			line_length = old_stash_len;
+		else
+			line_length = terminator_point - old_stashed_data;
 		*full_line = ft_strdup_len(old_stashed_data, line_length + 1);
-		new_stashed_data = ft_strdup_len(ft_strchr(old_stashed_data, '\n') + 1, ft_strlen(old_stashed_data));
+		new_stashed_data = ft_strdup_len(terminator_point + 1, (old_stash_len - line_length + 1));
 		free(old_stashed_data);
 		return (new_stashed_data);
 	}
@@ -42,6 +48,7 @@ char *get_next_line(int fd)
 
 	char *temp_reader;
 	int bytes_read;
+	int stash_len;
 
 	if(fd == -1)
 		return (NULL);
@@ -54,6 +61,7 @@ char *get_next_line(int fd)
 
 	while(!ft_strchr(stashed_data, '\n'))
 	{
+		printf("\n \t ==> gnl looping : [%s]", stashed_data);
 		bytes_read = read(fd, temp_reader, BUFFER_SIZE);
 		if(bytes_read <= 0)
 			break;
@@ -63,8 +71,10 @@ char *get_next_line(int fd)
 	}
 	if(stashed_data)
 	{
+		stash_len = ft_strlen(stashed_data);
+
 		printf("\n gnl  BEFR REFINE ==> %s ", stashed_data);
-		stashed_data = refine_line(stashed_data, &line_read);
+		stashed_data = refine_line(stashed_data, &line_read, stash_len);
 		printf("\n gnl  AFTR REFINE ==> %s ", stashed_data);
 	}
 	
@@ -72,24 +82,29 @@ char *get_next_line(int fd)
 	return(line_read);
 }
 
+
+
+
+
+
 int main()
 {
 	int fd;
-	char *line_read;
+	char *line_read = "";
 
 	fd = open("text.txt", O_RDONLY);
 	printf("\n== > file descriptor id : %d\n*****************\n\n", fd);
 
 	if(fd == -1)
 		return (0);
-	line_read = get_next_line(fd);
 	while(line_read)
 	{
-		printf("\n- [%s] -\n", line_read);
 		line_read = get_next_line(fd);
+		if(line_read)
+			printf("\n ##MAIN## \t\t LINE FOUND - [%s] -\n", line_read);
+		free(line_read);
 	}
 
-	free(line_read);
 	close(fd);
 
 	printf("\n \n== > file closed");
