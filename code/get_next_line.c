d6bfbd02dccd0a5f3afafa74efa12061ec57b5f8
Suhail <suhail542@gmail.com>
2025-12-25 01:31:51 +0400

    Norminette Completed

--- code/get_next_line.c
+++ code/get_next_line.c
@@ -6,17 +6,40 @@
 /*   By: ssujaude <ssujaude@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 15:17:45 by ssujaude          #+#    #+#             */
-/*   Updated: 2025/12/25 00:31:33 by ssujaude         ###   ########.fr       */
+/*   Updated: 2025/12/25 01:31:03 by ssujaude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*refine_line(char *old_stashed_data, char **full_line,
-		int old_stash_len)
+static char	*separate_line_from_stash(char *old_stashed_data,
+	char **full_line, char *terminator_pt, int old_stash_len)
 {
 	char	*new_stashed_data;
 	int		line_length;
+
+	if (!terminator_pt)
+	{
+		line_length = old_stash_len;
+		*full_line = ft_strdup_len(old_stashed_data, line_length);
+		free(old_stashed_data);
+		new_stashed_data = NULL;
+	}
+	else
+	{
+		line_length = terminator_pt - old_stashed_data;
+		*full_line = ft_strdup_len(old_stashed_data, line_length + 1);
+		new_stashed_data = ft_strdup_len(terminator_pt + 1, (old_stash_len
+					- (line_length + 1)));
+		free(old_stashed_data);
+		return (new_stashed_data);
+	}
+	return (NULL);
+}
+
+static char	*find_and_extract_line(char *old_stashed_data, char **full_line,
+		int old_stash_len)
+{
 	char	*terminator_point;
 
 	if (old_stashed_data)
@@ -29,43 +52,49 @@ char	*refine_line(char *old_stashed_data, char **full_line,
 		}
 		terminator_point = ft_strchr(old_stashed_data, '\n');
 		if (!terminator_point)
-		{
-			line_length = old_stash_len;
-			*full_line = ft_strdup_len(old_stashed_data, line_length);
-			free(old_stashed_data);
-			new_stashed_data = NULL;
-			return (new_stashed_data);
-		}
+			return (separate_line_from_stash(old_stashed_data, full_line,
+					terminator_point, old_stash_len));
 		else
-		{
-			line_length = terminator_point - old_stashed_data;
-			*full_line = ft_strdup_len(old_stashed_data, line_length + 1);
-			new_stashed_data = ft_strdup_len(terminator_point + 1,
-					(old_stash_len - (line_length + 1)));
-			free(old_stashed_data);
-			return (new_stashed_data);
-		}
+			return (separate_line_from_stash(old_stashed_data, full_line,
+					terminator_point, old_stash_len));
 	}
 	return (NULL);
 }
 
+static char	*post_read_processing(int bytes_read, char *stashed_data,
+		char **line_read)
+{
+	int	stash_len;
+
+	if (bytes_read == -1)
+	{
+		if (stashed_data)
+			free(stashed_data);
+		stashed_data = NULL;
+		return (NULL);
+	}
+	if (stashed_data)
+	{
+		stash_len = ft_strlen(stashed_data);
+		stashed_data = find_and_extract_line(stashed_data,
+				line_read, stash_len);
+	}
+	return (stashed_data);
+}
+
 char	*get_next_line(int fd)
 {
-	static char *stashed_data;
-	char *line_read;
-	char *temp_reader;
-	int bytes_read;
-	int stash_len;
+	static char	*stashed_data;
+	char		*line_read;
+	char		*temp_reader;
+	int			bytes_read;
 
 	line_read = NULL;
-
 	if ((fd < 0) || (BUFFER_SIZE <= 0))
 		return (NULL);
-
 	temp_reader = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!temp_reader)
 		return (NULL);
-
 	while (!ft_strchr(stashed_data, '\n'))
 	{
 		bytes_read = read(fd, temp_reader, BUFFER_SIZE);
@@ -77,17 +106,6 @@ char	*get_next_line(int fd)
 			break ;
 	}
 	free(temp_reader);
-	if (bytes_read == -1)
-	{
-		if (stashed_data)
-			free(stashed_data);
-		stashed_data = NULL;
-		return (NULL);
-	}
-	if (stashed_data)
-	{
-		stash_len = ft_strlen(stashed_data);
-		stashed_data = refine_line(stashed_data, &line_read, stash_len);
-	}
+	stashed_data = post_read_processing(bytes_read, stashed_data, &line_read);
 	return (line_read);
 }
--- code/get_next_line.h
+++ code/get_next_line.h
@@ -5,35 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ssujaude <ssujaude@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/06 15:17:55 by ssujaude          #+#    #+#             */
-/*   Updated: 2025/12/17 00:44:07 by ssujaude         ###   ########.fr       */
+/*   Created: 2025/12/25 01:23:21 by ssujaude          #+#    #+#             */
+/*   Updated: 2025/12/25 01:23:24 by ssujaude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 5
+#  define BUFFER_SIZE 5
 # endif
 
-# include <stdio.h>
-
 # include <fcntl.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-//check
 # include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_str_join_and_free(char *s1, char *s2);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup_len(const char *s, int copy_len);
 char	*ft_strchr(const char *s, int c);
-
 
 #endif
